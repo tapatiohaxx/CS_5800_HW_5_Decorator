a253d6166b53d3ba623fb126d585b7764b430eda
Matthew Plascencia <33331153+tapatiohaxx@users.noreply.github.com>
2024-11-05 11:18:41 -0800

    Update LoyaltyDiscountTests.java

--- test/java/Corg/CS5800/LoyaltyDiscountTests.java
+++ test/java/Corg/CS5800/LoyaltyDiscountTests.java
@@ -8,14 +8,14 @@ public class LoyaltyDiscountTests {
 
     @Test
     public void testApplyDiscount() {
-        LoyaltyDiscount discount = new LoyaltyDiscount(10);  // 10% discount
+        LoyaltyDiscount discount = new LoyaltyDiscount(10);  
         double result = discount.applyDiscount(100);
         assertEquals(90.0, result, 0.01, "10% discount on 100 should be 90.0");
     }
 
     @Test
     public void testNoDiscount() {
-        LoyaltyDiscount discount = new LoyaltyDiscount(0);  // 0% discount
+        LoyaltyDiscount discount = new LoyaltyDiscount(0); 
         double result = discount.applyDiscount(100);
         assertEquals(100.0, result, "0% discount on 100 should be 100.0");
     }
